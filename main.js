17ee30b8dd14d6b90c1654ce2da630ce99ffe8a5
Jeffrey <jeffrey.jr277@gmail.com>
2020-03-01 18:47:44 -0500

    Needle loops!

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ var powerOn = false;
 var tempo = 0;
 var beat = 0;
 var playing = false;
+var animateNeedle;
 
 function metrocolor(){
     var m = Snap("#Metronome .st0")
     m.attr({fill:'magenta'})
 }
+//bpm = tempo
+var beatPerSecond = (bpm) => {
+    return bpm / 60
+}
+
 //want to map st6 path color...
 function activate(){
     console.log('hi')
@@ -31,16 +37,16 @@ function setScreenColor(){
             screen.attr({fill: powerOn ? "green" : "orange"})
         });
 }
-function animateNeedle(){
+function play(){
+    let animateTime = 1000 / beatPerSecond(240)
+    animateNeedle = setInterval(function(){
     if (powerOn && playing)
-        {
+        {    
         let p = Snap("#Needle")
-        p.animate({x2: 418.5}, 500, function(){
-            p.animate({x2:218.5}, 1000, function(){
-                p.animate({x2:318.5}, 500)
-            })
-        })
-    }
+        p.animate({x2: 218.5}, animateTime, mina.bounce(),function(){
+        p.animate({x2:418.5}, animateTime,)})
+        }
+    },animateTime*2)
 }
 
 function playToggle(){
@@ -50,12 +56,13 @@ function playToggle(){
             p[1].attr({display:'none'})
             p[0].attr({display:'inline-block'})
             playing=false
+            clearInterval(animateNeedle)
         }
         else{
             p[0].attr({display:'none'})
             p[1].attr({display:'inline-block'})
+            play()
             playing=true
-            animateNeedle()
         }
     }
 }
