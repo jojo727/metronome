e5c9964ec469230ab2522a31d7c58b3a4772f7d2
jojo727 <strawbearrymilk@protonmail.com>
2020-03-03 23:58:53 -0500

    Tempo changes - added inc/dec support, needle animation speed based on the tempo value. need to figure out how to get the tempo text to display on screen...

--- main.js
+++ main.js
@@ -1,56 +1,74 @@
+
+//need to decide how to work with tempo
+//tempo inc/dec function needed plus how to display this in the associated box..
+//ALSO GET THAT ALARM CLOCK FONT!
 var powerOn = false;
-var tempo = 0;
+var tempo = 30;
 var beat = 0;
 var playing = false;
 var animateNeedle;
-
-function metrocolor(){
-    var m = Snap("#Metronome .st0")
-    m.attr({fill:'magenta'})
-}
+var tempoCount;
 //bpm = tempo
 var beatPerSecond = (bpm) => {
     return bpm / 60
 }
 
-//want to map st6 path color...
+
+
 function activate(){
     var power = [Snap("#PowerBttn"),Snap("#PowerBttnOn")]
-    var tempoCount = Snap("#TempoCount")
-    if (!powerOn){    
+    if (!powerOn){
+
         console.log('hi')
         powerOn = true;
         power[0].attr({display:'none'})
         power[1].attr({display:'inline-block'})
+        tempoCount = Snap("#TempoCount");
         tempoCount.attr({display:'inline-block'})
+        console.log(tempoCount.innerSVG)
         //Snap("#PowerBttn .st0").attr({display:'none'})
 
     }
     else{
         console.log("off")
-        console.log("000"+1)
+        console.log("0"+31)
         power[0].attr({display:'inline-block'})
         power[1].attr({display:'none'})
+        tempoCount.attr({display:'none'})
         if (playing){
             playToggle()
         }
         powerOn = false;
     }
 }
+function updateTempo(button){
+    if (powerOn){
+        console.log(tempo)
+        if (button == 'inc'){
+            if (tempo < 240){
+                tempoCount.innerSVG = tempo++
+            }
+ //           return tempo;
+        }
 
-function setScreenColor(){
-    let screens = [Snap(".st15"),Snap("#BeatTempoGroup .st8")]
-        screens.forEach(screen => { 
-            screen.attr({fill: powerOn ? "green" : "orange"})
-        });
+        else if (button == 'dec'){
+            if (tempo > 30){
+                tempoCount.innerSVG = tempo--
+            }
+//            return tempo
+        }
+        console.log(tempo)
+        displayTempo(tempo)
+    }
 }
+
 function play(){
-    let animateTime = 1000 / beatPerSecond(240)
+    let animateTime = 1000 / beatPerSecond(tempo)
     animateNeedle = setInterval(function(){
     if (powerOn && playing)
         {    
         let p = Snap("#Needle")
-        p.animate({x2: 218.5}, animateTime, mina.bounce(),function(){
+        p.animate({x2: 218.5}, animateTime, mina.easeinout(),function(){
         p.animate({x2:418.5}, animateTime,)})
         }
     },animateTime*2)
@@ -72,4 +90,9 @@ function playToggle(){
             playing=true
         }
     }
+}
+
+function displayTempo(bpm){
+    tempoCount.innerSVG = (bpm < 100) ? ("0" + bpm + "") : ("" + bpm + "")
+    return tempoCount.innerSVG
 }
